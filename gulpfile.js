f22af93f38fae7d444241d7aff5539228f5f71be
Oscar Bustos <oscarbustos@osoco.es>
2018-11-30 15:32:41 +0100

    data and languages supported

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cssnano = require('gulp-cssnano');
-var browserSync = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
-var pug = require('gulp-pug');
-var plumberNotifier = require('gulp-plumber-notifier');
-var imagemin = require('gulp-imagemin');
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  cssnano = require('gulp-cssnano'),
+  browserSync = require('browser-sync').create(),
+  autoprefixer = require('gulp-autoprefixer'),
+  pug = require('gulp-pug'),
+  plumberNotifier = require('gulp-plumber-notifier'),
+  imagemin = require('gulp-imagemin'),
+  data = require('gulp-data'),
+  fs = require('fs'),
+  path = require('path'),
+  merge = require('gulp-merge-json');
 
-gulp.task('default', ['css', 'js', 'sw', 'views', 'fonts'], () => {
+  // Declare Languages
+  var langs = ['es', 'en']
+
+gulp.task('default', ['data', 'css', 'js', 'sw', 'views', 'fonts'], () => {
   browserSync.init({
     server: "./dist"
   });
 
+  gulp.watch("src/data/**/*.json}", ['data']);
   gulp.watch("src/fonts/*}", ['fonts']);
   gulp.watch("src/service-workers/sw.js", ['sw']);
   gulp.watch("src/scripts/**/*.js", ['js']);
@@ -20,24 +28,66 @@ gulp.task('default', ['css', 'js', 'sw', 'views', 'fonts'], () => {
   gulp.watch("dist/**/*").on('change', browserSync.reload);
 });
 
+gulp.task('data', () => {
+  return gulp.src('src/data/**/*.json')
+    .pipe(merge({
+      fileName: 'data.json',
+      edit: (json, file) => {
+        // Extract the filename and strip the extension
+        var filename = path.basename(file.path),
+          primaryKey = filename.replace(path.extname(filename), '');
+        // Set the filename as the primary key for our JSON data
+        var data = {};
+        data[primaryKey.toUpperCase()] = json;
+        return data;
+      }
+    }))
+    .pipe(gulp.dest('src/data/'));
+});
+
 gulp.task('fonts', () => {
   gulp.src('src/fonts/*')
     .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('views', () =>
-  gulp.src('src/views/pages/*.pug')
-  .pipe(plumberNotifier())
-  .pipe(pug({
-    pretty: true
-  }))
-  .pipe(gulp.dest('dist/'))
-);
+gulp.task('views', ['data'], () => {
+
+  var firstLang = true
+  langs.forEach(function (lang) {
+    if (!firstLang) {
+      gulp.src('src/views/pages/*.pug')
+      .pipe(plumberNotifier())
+      .pipe(data((file) =>
+        JSON.parse(fs.readFileSync('src/data/data.json'))
+      ))
+      .pipe(pug({
+        pretty: true,
+        data: {lang:lang}
+      }))
+      .pipe(gulp.dest('dist/'+lang+'/'))      
+    } else {
+      gulp.src('src/views/pages/*.pug')
+      .pipe(plumberNotifier())
+      .pipe(data((file) =>
+        JSON.parse(fs.readFileSync('src/data/data.json'))
+      ))
+      .pipe(pug({
+        pretty: true,
+        data: {lang:lang}
+      }))
+      .pipe(gulp.dest('dist/'))      
+
+    }
+    firstLang = false
+  });
+
+
+});
 
 gulp.task('images', () =>
   gulp.src('src/img/*')
-  .pipe(imagemin())
-  .pipe(gulp.dest('dist/img/'))
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img/'))
 );
 
 gulp.task('sw', () => {
@@ -68,6 +118,7 @@ gulp.task('css', () => {
 });
 
 gulp.task('watch', () => {
+  gulp.watch('src/data/**/*.json', [data])
   gulp.watch('src/fonts/*', [fonts])
   gulp.watch('src/service-workers/sw.js', [sw]);
   gulp.watch('src/scripts/**/*.js', [js]);
